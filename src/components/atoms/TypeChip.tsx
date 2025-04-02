b3f0339443833da15d38f1dc5770e75451ea5cb1
jestruvec <jhonnyestruve@gmail.com>
2025-04-01 19:15:44 -0600

    se crea el helper para obtener color de acuerdo al tipo de pokemon

--- src/components/atoms/TypeChip.tsx
+++ src/components/atoms/TypeChip.tsx
@@ -1,4 +1,5 @@
 import { PokemonType } from "@/lib/types/Pokemon";
+import { GetColorByPokemonType } from "@/lib/helpers/GetColorByPokemonType";
 
 interface ComponentProps {
   label: string;
@@ -15,46 +16,11 @@ export const TypeChip = ({
   onClick,
   selected,
 }: ComponentProps) => {
-  const getColor = (): string => {
-    switch (type) {
-      case "normal":
-        return "bg-gray-400"; // Gris
-      case "grass":
-        return "bg-green-500"; // Verde
-      case "fire":
-        return "bg-red-500"; // Rojo
-      case "water":
-        return "bg-blue-500"; // Azul
-      case "bug":
-        return "bg-green-700"; // Verde oscuro
-      case "electric":
-        return "bg-yellow-500"; // Amarillo
-      case "rock":
-        return "bg-gray-600"; // Gris oscuro
-      case "ghost":
-        return "bg-purple-600"; // Púrpura
-      case "poison":
-        return "bg-purple-400"; // Púrpura claro
-      case "psychic":
-        return "bg-pink-500"; // Rosa
-      case "fighting":
-        return "bg-red-700"; // Rojo oscuro
-      case "ground":
-        return "bg-yellow-700"; // Amarillo oscuro
-      case "dragon":
-        return "bg-indigo-600"; // Índigo
-      case "flying":
-        return "bg-amber-500"; // Índigo
-      default:
-        return "bg-gray-200"; // Por defecto, gris claro
-    }
-  };
-
   return (
     <div
-      className={`rounded-full  hover:scale-105 transform ease-in-out duration-200 flex justify-center text-white font-bold border-gray-400 shadow-md p-1 text-sm ${getColor()} ${width} ${
-        onClick && "cursor-pointer"
-      } ${!selected && "opacity-50"}`}
+      className={`rounded-full  hover:scale-105 transform ease-in-out duration-200 flex justify-center text-white font-bold border-gray-400 shadow-md p-1 text-sm ${GetColorByPokemonType(
+        type
+      )} ${width} ${onClick && "cursor-pointer"} ${!selected && "opacity-50"}`}
       onClick={onClick}
     >
       {label}
